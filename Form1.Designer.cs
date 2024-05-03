90e08d5ac475fd15ed1f71c1545b12f5f9e75ab1
Giovanna B R Baratto <142364522+Bolsarim@users.noreply.github.com>
2024-05-02 21:51:42 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace monthCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDataInicial = new System.Windows.Forms.TextBox();
+            this.textBoxDataAtual = new System.Windows.Forms.TextBox();
+            this.textBoxDataFinal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar
+            // 
+            this.monthCalendar.Location = new System.Drawing.Point(18, 80);
+            this.monthCalendar.MaxSelectionCount = 30;
+            this.monthCalendar.Name = "monthCalendar";
+            this.monthCalendar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data Inicial:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data Final:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Data Atual:";
+            // 
+            // textBoxDataInicial
+            // 
+            this.textBoxDataInicial.Location = new System.Drawing.Point(453, 117);
+            this.textBoxDataInicial.Name = "textBoxDataInicial";
+            this.textBoxDataInicial.Size = new System.Drawing.Size(100, 26);
+            this.textBoxDataInicial.TabIndex = 4;
+            // 
+            // textBoxDataAtual
+            // 
+            this.textBoxDataAtual.Location = new System.Drawing.Point(453, 198);
+            this.textBoxDataAtual.Name = "textBoxDataAtual";
+            this.textBoxDataAtual.Size = new System.Drawing.Size(100, 26);
+            this.textBoxDataAtual.TabIndex = 6;
+            // 
+            // textBoxDataFinal
+            // 
+            this.textBoxDataFinal.Location = new System.Drawing.Point(453, 157);
+            this.textBoxDataFinal.Name = "textBoxDataFinal";
+            this.textBoxDataFinal.Size = new System.Drawing.Size(100, 26);
+            this.textBoxDataFinal.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Selecionar Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 493);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxDataFinal);
+            this.Controls.Add(this.textBoxDataAtual);
+            this.Controls.Add(this.textBoxDataInicial);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDataInicial;
+        private System.Windows.Forms.TextBox textBoxDataAtual;
+        private System.Windows.Forms.TextBox textBoxDataFinal;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
